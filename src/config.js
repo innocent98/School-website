f77695e63380cd0ad36a3d514ecd0d16e94eda9e
Innocent98 <tosinadebayo55@gmail.com>
2022-08-17 08:11:08 +0000

    updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://locate-auto-engineer.herokuapp.com/api/",
-  proxy: "http://localhost:5000/api/"
+  baseURL: "https://locate-auto-engineer.herokuapp.com/api/",
+  // proxy: "http://localhost:5000/api/"
 });
